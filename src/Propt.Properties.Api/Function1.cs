230af0cddb79b3c436284883c588c071ee7c3a21
Matt Hunt <mattlethargic@gmail.com>
2021-11-02 17:23:23 +0000

    fixing vanilla

--- src/Propt.Properties.Api/Function1.cs
+++ src/Propt.Properties.Api/Function1.cs
@@ -1,21 +1,22 @@
-using System.Linq;
-using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
-using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
 
 namespace Propt.Properties.Functions
 {
     public static class Function1
     {
         [FunctionName("Function1")]
-        public static async Task<HttpResponseMessage> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)]HttpRequestMessage req, TraceWriter log)
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)]HttpRequestMessage req, 
+            ILogger log)
         {
-            log.Info("C# HTTP trigger function processed a request.");
+            log.LogInformation("C# HTTP trigger function processed a request.");
 
-            return req.CreateResponse(HttpStatusCode.OK, "Hello");
+            return new OkObjectResult(new { message = "Hello" });
         }
     }
 }
